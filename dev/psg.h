3bc9e550f73cbcc8658475be18d59af2b9fe148a
StevePro7 <steveprozune@hotmail.com>
2018-03-15 08:31:40 +0000

    Initial commit

---
+++ dev/psg.h
@@ -0,0 +1,9 @@
+extern const unsigned char	music_psg[];
+#define				music_psg_size 7528
+
+extern const unsigned char	sfx_die_psg[];
+#define				sfx_die_psg_size 43
+
+extern const unsigned char	sfx_life_psg[];
+#define				sfx_life_psg_size 35
+
