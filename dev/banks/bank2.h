9de29b0cef17a6f86e0fec787fa87879cef5c8e5
StevePro7 <steveprozune@hotmail.com>
2018-03-15 18:44:51 +0000

    Add banks

---
+++ dev/banks/bank2.h
@@ -0,0 +1,8 @@
+extern const unsigned char	Bart__palette__bin[4][7];
+
+extern const unsigned char	Bart_OptionA[4][3];
+
+extern const unsigned char	Bart_OptionB[4][3];
+
+// 8-bit number
+extern const unsigned char Bart_Answer[4];
---
+++ dev/banks/bank3.h
@@ -0,0 +1,8 @@
+extern const unsigned char	Lisa__palette__bin[4][7];
+
+extern const unsigned char	Lisa_OptionA[4][3];
+
+extern const unsigned char	Lisa_OptionB[4][3];
+
+extern const unsigned char Lisa_Answer[4];
+//extern const unsigned int	Lisa_Answer[4];
