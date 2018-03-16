5b3ad663bb4c5740b1f573c52409bd8e58ed1d7c
StevePro7 <steveprozune@hotmail.com>
2018-03-16 12:15:33 +0000

    Update tile offsets

--- dev/gfx.h
+++ dev/gfx.h
@@ -23,7 +23,7 @@ extern const unsigned char	simpsons__palette__bin[];
 #define				simpsons__palette__bin_size 16
 
 extern const unsigned char	simpsons__tilemap__stmcompr[];
-#define				simpsons__tilemap__stmcompr_size 193
+#define				simpsons__tilemap__stmcompr_size 196
 
 extern const unsigned char	simpsons__tiles__psgcompr[];
 #define				simpsons__tiles__psgcompr_size 7783
