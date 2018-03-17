69de1771446acaecb559650d60412b205b4c43e4
StevePro7 <steveprozune@hotmail.com>
2018-03-17 13:15:51 +0000

    Update sprites

--- dev/engine/audio_manager.h
+++ dev/engine/audio_manager.h
@@ -3,25 +3,25 @@
 
 void engine_audio_manager_music()
 {
-	if (hacker_music)
+	if( hacker_music )
 	{
-		PSGPlayNoRepeat(MUSIC_PSG);
+		PSGPlayNoRepeat( MUSIC_PSG );
 	}
 }
 
 void engine_audio_manager_sound_right()
 {
-	if(hacker_sound)
+	if( hacker_sound )
 	{
-		PSGSFXPlay(SOUND1_PSG, SFX_CHANNEL2);
+		PSGSFXPlay( SOUND1_PSG, SFX_CHANNEL2 );
 	}
 }
 
 void engine_audio_manager_sound_wrong()
 {
-	if(hacker_sound)
+	if( hacker_sound )
 	{
-		PSGSFXPlay(SOUND2_PSG, SFX_CHANNEL2);
+		PSGSFXPlay( SOUND2_PSG, SFX_CHANNEL2 );
 	}
 }
 
