26a0203b3ec965f979e962f6b43a249f8d9b316d
StevePro7 <steveprozune@hotmail.com>
2018-03-20 19:33:31 +0000

    Add images

---
+++ dev/img.h
@@ -0,0 +1,18 @@
+extern const unsigned char	homer__palette__bin[];
+#define				homer__palette__bin_size 16
+
+extern const unsigned char	homer__tilemap__stmcompr[];
+#define				homer__tilemap__stmcompr_size 107
+
+extern const unsigned char	homer__tiles__psgcompr[];
+#define				homer__tiles__psgcompr_size 2765
+
+extern const unsigned char	marge__palette__bin[];
+#define				marge__palette__bin_size 16
+
+extern const unsigned char	marge__tilemap__stmcompr[];
+#define				marge__tilemap__stmcompr_size 100
+
+extern const unsigned char	marge__tiles__psgcompr[];
+#define				marge__tiles__psgcompr_size 1814
+
