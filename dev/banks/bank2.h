52cd3fde3a569284c5021947d8a7af746140f528
StevePro7 <steveprozune@hotmail.com>
2018-03-21 12:47:10 +0000

    Reduce quiz

--- dev/banks/bank2.h
+++ dev/banks/bank2.h
@@ -1,60 +1,64 @@
-extern const unsigned char bank2_quiz_line1[50][31];
-#define				bank2_quiz_line1_size 1550
-#define				bank2_quiz_line1_bank 50
+extern const unsigned char bank2_quiz_line1[10][31];
+#define				bank2_quiz_line1_size 310
+#define				bank2_quiz_line1_bank 2
 
-extern const unsigned char bank2_quiz_line2[50][31];
-#define				bank2_quiz_line2_size 1550
-#define				bank2_quiz_line2_bank 50
+extern const unsigned char bank2_quiz_line2[10][31];
+#define				bank2_quiz_line2_size 310
+#define				bank2_quiz_line2_bank 2
 
-extern const unsigned char bank2_quiz_line3[50][31];
-#define				bank2_quiz_line3_size 1550
-#define				bank2_quiz_line3_bank 50
+extern const unsigned char bank2_quiz_line3[10][31];
+#define				bank2_quiz_line3_size 310
+#define				bank2_quiz_line3_bank 2
 
-extern const unsigned char bank2_opt1_line1[50][18];
-#define				bank2_opt1_line1_size 900
-#define				bank2_opt1_line1_bank 50
+extern const unsigned char bank2_opt1_line1[10][18];
+#define				bank2_opt1_line1_size 180
+#define				bank2_opt1_line1_bank 2
 
-extern const unsigned char bank2_opt1_line2[50][18];
-#define				bank2_opt1_line2_size 900
-#define				bank2_opt1_line2_bank 50
+extern const unsigned char bank2_opt1_line2[10][18];
+#define				bank2_opt1_line2_size 180
+#define				bank2_opt1_line2_bank 2
 
-extern const unsigned char bank2_opt1_line3[50][18];
-#define				bank2_opt1_line3_size 900
-#define				bank2_opt1_line3_bank 50
+extern const unsigned char bank2_opt1_line3[10][18];
+#define				bank2_opt1_line3_size 180
+#define				bank2_opt1_line3_bank 2
 
-extern const unsigned char bank2_opt2_line1[50][18];
-#define				bank2_opt2_line1_size 900
-#define				bank2_opt2_line1_bank 50
+extern const unsigned char bank2_opt2_line1[10][18];
+#define				bank2_opt2_line1_size 180
+#define				bank2_opt2_line1_bank 2
 
-extern const unsigned char bank2_opt2_line2[50][18];
-#define				bank2_opt2_line2_size 900
-#define				bank2_opt2_line2_bank 50
+extern const unsigned char bank2_opt2_line2[10][18];
+#define				bank2_opt2_line2_size 180
+#define				bank2_opt2_line2_bank 2
 
-extern const unsigned char bank2_opt2_line3[50][18];
-#define				bank2_opt2_line3_size 900
-#define				bank2_opt2_line3_bank 50
+extern const unsigned char bank2_opt2_line3[10][18];
+#define				bank2_opt2_line3_size 180
+#define				bank2_opt2_line3_bank 2
 
-extern const unsigned char bank2_opt3_line1[50][18];
-#define				bank2_opt3_line1_size 900
-#define				bank2_opt3_line1_bank 50
+extern const unsigned char bank2_opt3_line1[10][18];
+#define				bank2_opt3_line1_size 180
+#define				bank2_opt3_line1_bank 2
 
-extern const unsigned char bank2_opt3_line2[50][18];
-#define				bank2_opt3_line2_size 900
-#define				bank2_opt3_line2_bank 50
+extern const unsigned char bank2_opt3_line2[10][18];
+#define				bank2_opt3_line2_size 180
+#define				bank2_opt3_line2_bank 2
 
-extern const unsigned char bank2_opt3_line3[50][18];
-#define				bank2_opt3_line3_size 900
-#define				bank2_opt3_line3_bank 50
+extern const unsigned char bank2_opt3_line3[10][18];
+#define				bank2_opt3_line3_size 180
+#define				bank2_opt3_line3_bank 2
 
-extern const unsigned char bank2_opt4_line1[50][18];
-#define				bank2_opt4_line1_size 900
-#define				bank2_opt4_line1_bank 50
+extern const unsigned char bank2_opt4_line1[10][18];
+#define				bank2_opt4_line1_size 180
+#define				bank2_opt4_line1_bank 2
 
-extern const unsigned char bank2_opt4_line2[50][18];
-#define				bank2_opt4_line2_size 900
-#define				bank2_opt4_line2_bank 50
+extern const unsigned char bank2_opt4_line2[10][18];
+#define				bank2_opt4_line2_size 180
+#define				bank2_opt4_line2_bank 2
 
-extern const unsigned char bank2_opt4_line3[50][18];
-#define				bank2_opt4_line3_size 900
-#define				bank2_opt4_line3_bank 50
+extern const unsigned char bank2_opt4_line3[10][18];
+#define				bank2_opt4_line3_size 180
+#define				bank2_opt4_line3_bank 2
+
+extern const unsigned char bank2_soln[10];
+#define				bank2_soln_size 10
+#define				bank2_soln_bank 2
 
---
+++ dev/screen/test5_screen.h
@@ -0,0 +1,28 @@
+#ifndef _TEST5_SCREEN_H_
+#define _TEST5_SCREEN_H_
+
+extern unsigned int screen_bases_screen_count;
+extern unsigned int screen_bases_screen_timer;
+extern unsigned char screen_test5_screen_delay;
+
+void screen_test5_screen_init()
+{
+	screen_test5_screen_delay = 100;
+}
+
+void screen_splash_screen_load()
+{
+	SMS_displayOff();
+	screen_bases_screen_init();
+	SMS_displayOn();
+}
+
+void screen_test5_screen_update(unsigned char *screen_type, unsigned int curr_joypad1, unsigned int prev_joypad1)
+{
+	unsigned int test_curr_joypad1 = curr_joypad1;
+	unsigned int test_prev_joypad1 = prev_joypad1;
+
+	*screen_type = SCREEN_TYPE_TEST5;
+}
+
+#endif//_TEST5_SCREEN_H_
