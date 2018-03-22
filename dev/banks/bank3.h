e382b48e3d97dad55599175a82e148f815dc9747
StevePro7 <steveprozune@hotmail.com>
2018-03-22 18:08:50 +0000

    Update bank3 with questions

--- dev/banks/bank3.h
+++ dev/banks/bank3.h
@@ -1,60 +1,60 @@
-extern const unsigned char bank3_quiz_line1[50][31];
-#define				bank3_quiz_line1_size 1550
-#define				bank3_quiz_line1_bank 50
+extern const unsigned char bank3_quiz_line1[10][31];
+#define				bank3_quiz_line1_size 310
+#define				bank3_quiz_line1_bank 3
 
-extern const unsigned char bank3_quiz_line2[50][31];
-#define				bank3_quiz_line2_size 1550
-#define				bank3_quiz_line2_bank 50
+extern const unsigned char bank3_quiz_line2[10][31];
+#define				bank3_quiz_line2_size 310
+#define				bank3_quiz_line2_bank 3
 
-extern const unsigned char bank3_quiz_line3[50][31];
-#define				bank3_quiz_line3_size 1550
-#define				bank3_quiz_line3_bank 50
+extern const unsigned char bank3_quiz_line3[10][31];
+#define				bank3_quiz_line3_size 310
+#define				bank3_quiz_line3_bank 3
 
-extern const unsigned char bank3_opt1_line1[50][18];
-#define				bank3_opt1_line1_size 900
-#define				bank3_opt1_line1_bank 50
+extern const unsigned char bank3_opt1_line1[10][18];
+#define				bank3_opt1_line1_size 180
+#define				bank3_opt1_line1_bank 3
 
-extern const unsigned char bank3_opt1_line2[50][18];
-#define				bank3_opt1_line2_size 900
-#define				bank3_opt1_line2_bank 50
+extern const unsigned char bank3_opt1_line2[10][18];
+#define				bank3_opt1_line2_size 180
+#define				bank3_opt1_line2_bank 3
 
-extern const unsigned char bank3_opt1_line3[50][18];
-#define				bank3_opt1_line3_size 900
-#define				bank3_opt1_line3_bank 50
+extern const unsigned char bank3_opt1_line3[10][18];
+#define				bank3_opt1_line3_size 180
+#define				bank3_opt1_line3_bank 3
 
-extern const unsigned char bank3_opt2_line1[50][18];
-#define				bank3_opt2_line1_size 900
-#define				bank3_opt2_line1_bank 50
+extern const unsigned char bank3_opt2_line1[10][18];
+#define				bank3_opt2_line1_size 180
+#define				bank3_opt2_line1_bank 3
 
-extern const unsigned char bank3_opt2_line2[50][18];
-#define				bank3_opt2_line2_size 900
-#define				bank3_opt2_line2_bank 50
+extern const unsigned char bank3_opt2_line2[10][18];
+#define				bank3_opt2_line2_size 180
+#define				bank3_opt2_line2_bank 3
 
-extern const unsigned char bank3_opt2_line3[50][18];
-#define				bank3_opt2_line3_size 900
-#define				bank3_opt2_line3_bank 50
+extern const unsigned char bank3_opt2_line3[10][18];
+#define				bank3_opt2_line3_size 180
+#define				bank3_opt2_line3_bank 3
 
-extern const unsigned char bank3_opt3_line1[50][18];
-#define				bank3_opt3_line1_size 900
-#define				bank3_opt3_line1_bank 50
+extern const unsigned char bank3_opt3_line1[10][18];
+#define				bank3_opt3_line1_size 180
+#define				bank3_opt3_line1_bank 3
 
-extern const unsigned char bank3_opt3_line2[50][18];
-#define				bank3_opt3_line2_size 900
-#define				bank3_opt3_line2_bank 50
+extern const unsigned char bank3_opt3_line2[10][18];
+#define				bank3_opt3_line2_size 180
+#define				bank3_opt3_line2_bank 3
 
-extern const unsigned char bank3_opt3_line3[50][18];
-#define				bank3_opt3_line3_size 900
-#define				bank3_opt3_line3_bank 50
+extern const unsigned char bank3_opt3_line3[10][18];
+#define				bank3_opt3_line3_size 180
+#define				bank3_opt3_line3_bank 3
 
-extern const unsigned char bank3_opt4_line1[50][18];
-#define				bank3_opt4_line1_size 900
-#define				bank3_opt4_line1_bank 50
+extern const unsigned char bank3_opt4_line1[10][18];
+#define				bank3_opt4_line1_size 180
+#define				bank3_opt4_line1_bank 3
 
-extern const unsigned char bank3_opt4_line2[50][18];
-#define				bank3_opt4_line2_size 900
-#define				bank3_opt4_line2_bank 50
+extern const unsigned char bank3_opt4_line2[10][18];
+#define				bank3_opt4_line2_size 180
+#define				bank3_opt4_line2_bank 3
 
-extern const unsigned char bank3_opt4_line3[50][18];
-#define				bank3_opt4_line3_size 900
-#define				bank3_opt4_line3_bank 50
+extern const unsigned char bank3_opt4_line3[10][18];
+#define				bank3_opt4_line3_size 180
+#define				bank3_opt4_line3_bank 3
 
